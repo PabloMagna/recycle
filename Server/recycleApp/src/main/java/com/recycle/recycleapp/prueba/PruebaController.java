b88502b11cb7f6a2a6b14a9597ab57ca885c942d
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2024-05-31 10:33:19 -0300

    feat: security

--- Server/recycleApp/src/main/java/com/recycle/recycleapp/prueba/PruebaController.java
+++ Server/recycleApp/src/main/java/com/recycle/recycleapp/prueba/PruebaController.java
@@ -1,25 +1,31 @@
 package com.recycle.recycleapp.prueba;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PruebaController {
     @Autowired
-    private PruebaRepository repository;
+    private PruebaService service;
 
     @PostMapping("/ingreso")
-
-    public String sumar(@RequestBody Prueba prueba){
-        repository.save(prueba);
+    @PreAuthorize("hasRole('ADMIN')")
+    public String ingreso(@RequestBody Prueba prueba, Authentication jwt){
+        service.ingresoTexto(prueba,jwt);
         return "exito";
     }
 
     @GetMapping("{id}")
-    @PreAuthorize("hasRole('ROLE_admin')")
-    public String traerPrueba(@PathVariable Integer id){
-        Prueba prueba = repository.findById(id).orElse(null);
-        return prueba.getP();
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Prueba> traerPrueba(@PathVariable Integer id, Authentication jwt) {
+        Prueba prueba = service.traerPrueba(id, jwt);
+        if (prueba != null) {
+            return ResponseEntity.ok(prueba);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 }
